84583a466c8d0f40656696ebf58610f0be6bc7d0
mrmojo2 <maobadi360@gmail.com>
2025-05-15 16:30:11 +0545

    lexer updated for two char tokens

--- src/Lexer.cpp
+++ src/Lexer.cpp
@@ -21,22 +21,40 @@ void Lexer::tokenize(){
 		else if(ch == '+')	add_token(TOK_PLUS);
 		else if(ch == '-')	add_token(TOK_MINUS);
 		else if(ch == '*')	add_token(TOK_STAR);
+		else if(ch == '/')	add_token(TOK_SLASH);
+		else if(ch == ':')	add_token(TOK_COLON);
+		else if(ch == ';')	add_token(TOK_SEMICOLON);
+		else if(ch == ',')	add_token(TOK_COMMA);
+		else if(ch == '.')	add_token(TOK_DOT);
+		else if(ch == '%')	add_token(TOK_MOD);
+		else if(ch == '?')	add_token(TOK_QUES);
 		else if(ch == '(')	add_token(TOK_LPAREN);
 		else if(ch == ')')	add_token(TOK_RPAREN);
+		else if(ch == '[')	add_token(TOK_LSQ);
+		else if(ch == ']')	add_token(TOK_RSQ);
+		else if(ch == '{')	add_token(TOK_LCURLY);
+		else if(ch == '}')	add_token(TOK_RCURLY);
 
 		//TODO:fill this for all other single char tokens
 		
 		else if(is_num(ch)){
-			while(is_num(lookahead())) advance();
-			advance();
+			while(is_num(peek())){ advance();}
 			add_token(TOK_INTEGER);
 		}else if(ch == '>'){
-			if(lookahead() == '='){
-				advance();
-				add_token(TOK_GE);
-			}else{
-				add_token(TOK_GT);
-			}
+			if(match('=')) 		add_token(TOK_GE);
+			else if(match('>'))	add_token(TOK_GTGT);
+			else			add_token(TOK_GT);
+			
+		}else if(ch == '<'){
+			if(match('='))		add_token(TOK_LE);
+			else if(match('<'))	add_token(TOK_LTLT);
+			else			add_token(TOK_LT);
+		}else if(ch == '='){
+			if(match('='))		add_token(TOK_EQ);
+			else 			add_token(TOK_ASSIGN);
+		}else if(ch == '~'){
+			if(match('='))		add_token(TOK_NE);
+			else			add_token(TOK_NOT);
 		}
 	}
 }
@@ -56,7 +74,9 @@ char Lexer::peek(){
 }
 
 char Lexer::lookahead(){
-	return source[current_pointer + 1];	
+	if(current_pointer + 1 < source.length())
+		return source[current_pointer + 1];
+	return '\0';	
 }
 
 bool Lexer::match(char expected){
