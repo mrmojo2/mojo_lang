974c3f81c36b4627aa09f3617907d7dd2d0e1335
mrmojo2 <maobadi360@gmail.com>
2025-05-27 08:00:56 +0545

    changed testscript

--- testscript2.ml
+++ testscript2.ml
@@ -1,9 +1,4 @@
-# initialize variable
-pi = 3.141592
-x = 4.5
-
-if x>=0 then
-        println('x is positive')
-else
-        println('x is negative')
-end
+#####################################
+##      Parsing Expressions
+####################################
+2+42*2+(47 * - 21)
